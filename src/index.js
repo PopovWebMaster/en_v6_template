77bcb3d2c385c7348c64a8c78b1c92fe8715de0c
PopovWebMaster <popovwebmaster84pro@mail.ru>
2019-08-26 15:16:32 +0300

    commit 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//import './js/common';
+//import './css/main.css';
+//import './scss/main.scss';
+
+import './css/css_fontello/animation.css';
+import './css/css_fontello/fontello.css';
+import './css/css_fontello/fontello-codes.css';
+import './css/css_fontello/fontello-embedded.css';
+import './css/css_fontello/fontello-ie7.css';
+import './css/css_fontello/fontello-ie7-codes.css';
+
+
