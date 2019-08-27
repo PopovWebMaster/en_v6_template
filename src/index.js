273359c713ba2bbed535df608c81975fbd95d2fd
PopovWebMaster <popovwebmaster84pro@mail.ru>
2019-08-27 15:14:05 +0300

    header leftMenu honePage footer (без javaScript)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 //import './js/common';
 //import './css/main.css';
-//import './scss/main.scss';
 
 import './css/css_fontello/animation.css';
 import './css/css_fontello/fontello.css';
@@ -9,4 +8,6 @@ import './css/css_fontello/fontello-embedded.css';
 import './css/css_fontello/fontello-ie7.css';
 import './css/css_fontello/fontello-ie7-codes.css';
 
-var a = 1;
+import './scss/main.scss'; 
+
+
